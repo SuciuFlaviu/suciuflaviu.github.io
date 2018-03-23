97e56745017f17156763b84fe6589cde90a5a4fb
SuciuFlaviu <flaviusuciuflorian@gmail.com>
2018-03-23 18:18:13 +0200

    ver 2

--- ball.js
+++ ball.js
@@ -1,21 +1,21 @@
 
-document.getElementById("id_business_level").innerHTML = "Business version = 2018.03.23.1";
+document.getElementById("id_business_level").innerHTML = "Bussiness version = 2018.03.23.2";
+
+var stare = {y:0, directie: 1};
+
+setInterval(deseneaza, 30, stare);
 
 
 
-setInterval(deseneaza, 30, 0, 1);
 
-
-
-
-function deseneaza()
+function deseneaza(x)
 {
 	vat t = document.getElementById("id_transform");
-	t.setAttribute("translation","0 " + y + "0 ");
-	y++
-	if (y>10)
-		directie = -1;
+	t.setAttribute("translation","0 " + x.y + " 0");
+	x.y += x.directie;
+	if (x.y>10)
+		x.directie = -1;
 	else
-		if (y<0);
-			directie = 1;
+		if (x.y<0);
+			x.directie = 1;
 }
